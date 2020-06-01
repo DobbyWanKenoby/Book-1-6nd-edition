df316ab48ff737222e3930550d79a45360eaf8a5
Vasiliy Usov <alkal@ya.ru>
2020-06-01 16:22:27 +0300

    Add 37Chapter App

---
+++ 37 MyName/MyName/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MyName
+//
+//  Created by Василий Усов on 06.04.2020.
+//  Copyright © 2020 Василий Усов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func showMessage(){
+         print("you press Hello World button")
+    }
+
+}
